5571e5a61693217b61107583fd1c3283c2d5d7b6
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-14 12:59:57 +0300

    feat: add get post put delete api

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express();
-const artists = [
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+let artists = [
     {
         id: 1,
         name: "John"
@@ -14,11 +19,33 @@ const artists = [
         name: "Petro"
     }
 ]
+
 app.get('/', (req, res) => { res.send('Hello API') })
 app.get('/artists', (req, res) => { res.send(artists) })
 app.get('/artists/:id', (req, res) => { 
-   console.log(req.params);
+   console.log(req.params.id);
    let artist =artists.find(i=>i.id===+req.params.id)
-   res.send(artist.name) })
+   res.send(artist) })
+
+   app.post('/artists', (req, res)=> {
+    let artist = {
+    id: Date.now(),
+    name: req.body.name
+    };
+    artists.push(artist);
+    res.send(artist);
+    })
+
+app.put( '/artists/:id', (req, res)=> {
+   let artist = artists.find( i =>i.id === +req.params.id)
+    artist.name = req.body.name;
+    res.send(artist);
+    })
+
+app.delete('/artists/:id',(req, res)=> {
+    artists = artists.filter(i=> i.id !== +req.params.id)
+    res.send(artists);
+        })
+
 app.listen(8080);
 console.log('App started! Look at http://localhost:8080');
